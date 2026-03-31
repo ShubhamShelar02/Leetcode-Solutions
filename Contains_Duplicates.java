6d2ef60be2c25b1c641830f751490d8cb19d8268
Shubham Shelar <shelarshubham8691@gmail.com>
2026-03-31 11:12:03 +0530

    refractor: Contains_Duplicates.java

--- Contains_Duplicates.java
+++ Contains_Duplicates.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         for(int num : nums){
-            if(hs.contains(num){
+            if(hs.contains(num)){
+
                 return true;
             }
             hs.add(num);
