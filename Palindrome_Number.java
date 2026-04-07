cf331123dd455b3ae8ca487f4abd3fcd1627ed11
Shubham Shelar <shelarshubham8691@gmail.com>
2026-04-07 13:13:11 +0530

    initial message

---
+++ Palindrome_Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x){
+        int reverse = 0;
+        int n = x;
+        while (n > 0){
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n /= 10;
+        }
+        if (reverse != x){
+            return false;
+        }
+        return true;
+    }
